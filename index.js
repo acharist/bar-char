39b6283349b23ff7621096b7792c9d82b414e5c4
acharist <teyanesst@gmail.com>
2021-12-05 00:08:19 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,90 +1,87 @@
 (function () {
+  // Init app
+  (function init() {
+    const config = {
+      method: "GET",
+      cache: "no-cache",
+      headers: {
+        "content-type": "application/json",
+      },
+    };
 
-    //Init app
-    (function init() {
-        const config = {
-            method: 'GET',
-            cache: 'no-cache',
-            headers: {
-                'content-type': 'application/json'
-            },
-        }
+    fetchData(
+      "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json",
+      config
+    );
+  })();
 
-        fetchData('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', config);
-    })();
+  // Fetch data from url
+  function fetchData(url) {
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        app(res.data);
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
 
-    //Fetch data from url
-    function fetchData(url, config) {
-        fetch(url)
-            .then(res => {
-                return res.json();
-            })
-            .then(res => {
-                app(res.data);
-            })
-            .catch(err => {
-                return err;
-            })
+  // Main app function
+  function app(fetchedData) {
+    const forElements = document.querySelector(".data-cont");
+
+    const amountOfItems = fetchedData.length;
+    const width = +(100 / amountOfItems).toFixed(2);
+
+    fetchedData.map((item, index) => {
+      let height = item[1] / 30;
+      let uiElement = elementFromData(item[0], item[1], width, height);
+      forElements.appendChild(uiElement);
+    });
+
+    function pointing(event) {
+      const target = event.target;
+
+      if (target.classList.contains("item")) {
+        target.children[0].classList.toggle("block");
+      }
     }
 
-    //Main app function
-    function app(fetchedData) {
+    forElements.addEventListener("mouseover", pointing);
 
-        const forElements = document.querySelector('.data-cont');
+    forElements.addEventListener("mouseout", pointing);
+  }
 
-        const amountOfItems = fetchedData.length;
-        const width = +(100 / amountOfItems).toFixed(2);
+  // Help functions
+  function elementFromData(date, price, width, height) {
+    // Item root element
+    const uiItem = document.createElement("div");
+    uiItem.className = "item";
+    uiItem.style.height = height + "px";
 
-        fetchedData.map((item, index) => {
-            let height = item[1] / 30;
-            let uiElement = elementFromData(item[0], item[1], width, height);
-            forElements.appendChild(uiElement)
-        });
+    // Container for item information
+    const uiItemCont = document.createElement("div");
+    uiItemCont.className = "item-cont";
 
-        function pointing(event) {
-            const target = event.target;
+    const uiPrice = document.createElement("div");
+    uiPrice.classNAme = "price";
+    uiPrice.textContent = `$${price} Bilion`;
 
-            if(target.classList.contains('item')) {
-                target.children[0].classList.toggle('block')
-            }
-        }
+    const uiDate = document.createElement("div");
+    uiDate.textContent = date;
+    uiDate.className = "date";
 
+    uiItemCont.appendChild(uiPrice);
+    uiItemCont.appendChild(uiDate);
+    uiItem.appendChild(uiItemCont);
 
-        forElements.addEventListener('mouseover', pointing);
+    return uiItem;
+  }
 
-        forElements.addEventListener('mouseout', pointing);
-
-    }
-
-    //Help functions
-    function elementFromData(date, price, width, height) {
-        //Item root element
-        const uiItem = document.createElement('div');
-        uiItem.className = 'item';
-        // uiItem.style.width = width + '%';
-        uiItem.style.height = height + 'px';
-
-        //Container for item information
-        const uiItemCont = document.createElement('div');
-        uiItemCont.className = 'item-cont';
-
-        const uiPrice = document.createElement('div');
-        uiPrice.classNAme = 'price';
-        uiPrice.textContent = `$${price} Bilion`;
-
-        const uiDate = document.createElement('div');
-        uiDate.textContent = date;
-        uiDate.className = 'date';
-
-        uiItemCont.appendChild(uiPrice);
-        uiItemCont.appendChild(uiDate);
-        uiItem.appendChild(uiItemCont);
-
-        return uiItem;
-    }
-
-    function getRandomArbitrary(min, max) {
-        return Math.random() * (max - min) + min;
-    }
-
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 })();
